6844c2a0b79cfbc11c89dde9e77b5026db22bf72
Avval Halani <aahalani@gmail.com>
2024-02-26 14:39:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,65 +3,85 @@ from streamlit_chat import message
 from dotenv import load_dotenv
 import os
 import json
-
+import streamlit.components.v1 as components  
+from pymongo import MongoClient
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import (
     SystemMessage,
     HumanMessage,
     AIMessage
 )
+from datetime import datetime
 
-# Function to check the user login (placeholder for real authentication)
-def check_login(username, password, registered_users):
-    # Placeholder for user validation (replace with real validation logic)
-    for user in registered_users:
-        if user['username'] == username and user['password'] == password:
-            return True
-    return False
-
-# Function to register a new user
-def register_user(username, password):
-    # Placeholder for user registration (replace with real registration logic)
-    # Store the user credentials in a dictionary
-    user_credentials = {
-        'username': username,
-        'password': password
-    }
-    return user_credentials
-
-# Function to serialize chat messages to JSON-serializable format
+# Function to serialize chat messages to JSON-serializable format with timestamps
 def serialize_chat_messages(chat_history):
     serialized_messages = []
     for message in chat_history:
         if isinstance(message, SystemMessage):
-            serialized_messages.append({'type': 'system', 'content': str(message.content)})
+            serialized_messages.append({'type': 'system', 'content': str(message.content), 'timestamp': datetime.now()})
         elif isinstance(message, HumanMessage):
-            serialized_messages.append({'type': 'human', 'content': str(message.content)})
+            serialized_messages.append({'type': 'human', 'content': str(message.content), 'timestamp': datetime.now()})
         elif isinstance(message, AIMessage):
-            serialized_messages.append({'type': 'ai', 'content': str(message.content)})
+            serialized_messages.append({'type': 'ai', 'content': str(message.content), 'timestamp': datetime.now()})
     return serialized_messages
 
-# Function to save chat history to a file
+
+MONGO_URL="mongodb+srv://avvalhalani:1234@db.aeoo5eo.mongodb.net/?retryWrites=true&w=majority&appName=DB"
+client = MongoClient(MONGO_URL)
+db = client.DB_Dis
+
+# MongoDB collections
+users_collection = db.users
+chat_history_collection = db.chat_history
+submissions_collection = db.submissions
+
+# Function to check the user login (placeholder for real authentication)
+def check_login(username, password):
+    user = users_collection.find_one({'username': username, 'password': password})
+    return user is not None
+
+# Function to register a new user
+def register_user(username, password):
+    user_credentials = {
+        'username': username,
+        'password': password,
+        'language': 'Marathi',
+        'created_at': datetime.now()
+    }
+    users_collection.insert_one(user_credentials)
+    return user_credentials
+
+
+def handle_submit(question, text):
+    # Collect data from text inputs
+    input_data = {
+        'username': st.session_state['user_id'],
+        'question': question,
+        'answer': text,
+        'timestamp': datetime.now()
+    }
+
+    # Save submission data to MongoDB
+    save_submission(st.session_state['user_id'], input_data)
+
+    st.success("Data submitted successfully!")
+
+# Function to save chat history to MongoDB
 def save_chat_history(user_id, chat_history):
-    chat_file = f"chat_history_{user_id}.json"
     serialized_chat = serialize_chat_messages(chat_history)
-    with open(chat_file, 'w') as file:
-        json.dump(serialized_chat, file)
+    chat_history_collection.update_one(
+        {'user_id': user_id},
+        {'$set': {'chat_history': serialized_chat}},
+        upsert=True
+    )
 
-# Function to save registered users to a file
-def save_registered_users(registered_users):
-    users_file = "registered_users.json"
-    with open(users_file, 'w') as file:
-        json.dump(registered_users, file)
+# Function to save submissions to MongoDB
+def save_submission(user_id, submission_data):
+    submissions_collection.insert_one({
+        'user_id': user_id,
+        'submissions': submission_data
+    })
 
-# Function to fetch registered users from a file
-def fetch_registered_users():
-    users_file = "registered_users.json"
-    if os.path.exists(users_file):
-        with open(users_file, 'r') as file:
-            return json.load(file)
-    else:
-        return []
 
 def init():
     load_dotenv()
@@ -77,28 +97,50 @@ def init():
     )
 
 SystemPrompt = "You are a translating bot. "
-# Who is tasked with translating English to Marathi. You are not allowed to translate technical terms such as C++, Hash function, etc. this is done to ensure the meaning of the text is not lost."
-def gptModel(input_user):
-    chat = ChatOpenAI(model_name="gpt-4-1106-preview", temperature=0)
-    messages = [
-        SystemMessage(content=f"{SystemPrompt}"),
-        HumanMessage(content=
-                    f"""Translate the phrase "{input_user}" to Marathi. However leave the technical programming terms in English and translate generic English words to Marathi. Do not translate the"""),
-    ]
-    response = chat(messages)
-    # Log input and output to a text file
-    # with open('log.txt', 'a') as log_file:
-    #     log_file.write(f"Input to the LLM Model:\n System Prompt:{SystemPrompt}\n User Input:{input_user}\n")
-    #     log_file.write(f"Output of the LLM Model:\n{response.content}\n")
-    #     log_file.write("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n")
 
+def update_language_preference(username, language):
+    users_collection.update_one(
+        {'username': username},
+        {'$set': {'language': language}},
+        upsert=True
+    )
+
+
+def gptModel(input_user, language, user_id):
+    update_language_preference(user_id, language)
+    if language == 'Marathi':
+        chat = ChatOpenAI(model_name="gpt-4-1106-preview", temperature=0)
+        messages = [
+            SystemMessage(content=f"{SystemPrompt}"),
+            HumanMessage(content=
+                        f"""
+                        You are mentoring a student who is trying to solve the following programming problem:\n\"{questionDescription}\"\n"
+                        The student will ask questions related to the problem. DO NOT OUTPUT THE SOLUTION OF THE ENTIRE PROBLEM. You can provide hints and suggestions to their questions. If they have questions related to programming concepts, you can answer them by providing snippets of code.\n
+                        Only provide answers if it is related to the question.\n
+                        Provide the answer in Marathi\n
+                        The student's question is: {input_user}
+                        """),
+        ]
+    elif language == 'English':
+        chat = ChatOpenAI(model_name="gpt-4-1106-preview", temperature=0)
+        messages = [
+            SystemMessage(content=f"{SystemPrompt}"),
+            HumanMessage(content=
+                        f"""
+                        You are mentoring a student who is trying to solve the following programming problem:"{questionDescription}
+                        The student will ask questions related to the problem. DO NOT OUTPUT THE SOLUTION OF THE ENTIRE PROBLEM. You can provide hints and suggestions to their questions. If they have questions related to programming concepts, you can answer them by providing snippets of code.\n
+                        Only provide answers if it is related to the question.\n
+                        Provide the answer in Marathi\n
+                        The student's question is: {input_user}
+
+                        """),
+        ]
+    response = chat(messages)
     return response.content
 
 def main():
     init()
-
     # Fetch registered users
-    registered_users = fetch_registered_users()
 
     # Login and Registration form
     if 'logged_in' not in st.session_state or not st.session_state['logged_in']:
@@ -108,15 +150,16 @@ def main():
             new_password = st.sidebar.text_input("New Password", type="password")
             if st.sidebar.button("Register"):
                 user_credentials = register_user(new_username, new_password)
-                registered_users.append(user_credentials)
-                save_registered_users(registered_users)  # Save the updated list
-                st.session_state['registered_users'] = registered_users
+                # registered_users.append(user_credentials)
+                # save_registered_users(registered_users)  # Save the updated list
+                # st.session_state['registered_users'] = registered_users
                 st.sidebar.success("Registration successful. You can now login.")
+            
         else:
             username = st.sidebar.text_input("Username")
             password = st.sidebar.text_input("Password", type="password")
             if st.sidebar.button("Login"):
-                if check_login(username, password, registered_users):
+                if check_login(username, password):
                     st.session_state['logged_in'] = True
                     st.session_state['user_id'] = username
                 else:
@@ -131,27 +174,141 @@ def main():
             ]
 
         st.header(f"GPT Helper (User: {user_id})")
+        st.sidebar.title("Navigation")
+        page = st.sidebar.radio("Go to", ["Question 1", "Question 2", "Question 3"])
+
+        language = st.sidebar.selectbox("Select Language", ['Marathi', 'English'])
+
+        if page == "Question 1":
+            st.subheader("Question 1")
+            st.image("./image 1.png", caption="Image 1")
+            components.html(
+                """
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <title>Hello, World!</title>
+                        <style>
+                            body {
+                                display: flex;
+                                justify-content: flex-start;
+                            }
+                            iframe {
+                                margin: 0;
+                            }
+                        </style>
+                    </head>
+                    <body>
+                        <iframe width="700px" height="500px" src="https://www.programiz.com/c-programming/online-compiler/"></iframe>
+                    </body>
+                </html>
+                """,
+                height=500,
+                width=700,
+            )
+        
+            fetch_latest_submission = submissions_collection.find_one({'user_id': user_id, 'submissions.question': 'Question 1'}, sort=[('submissions.timestamp', -1)])
+            if fetch_latest_submission:
+                st.text_area("Your previous answer:", value=fetch_latest_submission['submissions']['answer'], height=100, key="text_area_Question 1")
+            else:
+                st.text_area("Enter your text here:", key="text_area_Question 1", height=100)
+
+        elif page == "Question 2":
+            st.subheader("Question 2")
+            st.image("./image 2.png", caption="Image 2")
+            components.html(
+                """
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <title>Hello, World!</title>
+                        <style>
+                            body {
+                                display: flex;
+                                justify-content: flex-start;
+                            }
+                            iframe {
+                                margin: 0;
+                            }
+                        </style>
+                    </head>
+                    <body>
+                        <iframe width="700px" height="500px" src="https://www.programiz.com/c-programming/online-compiler/"></iframe>
+                    </body>
+                </html>
+                """,
+                height=500,
+                width=700,
+            )
+            fetch_latest_submission = submissions_collection.find_one({'user_id': user_id, 'submissions.question': 'Question 2'}, sort=[('submissions.timestamp', -1)])
+            if fetch_latest_submission:
+                st.text_area("Your previous answer:", value=fetch_latest_submission['submissions']['answer'], height=100, key="text_area_Question 2")
+            else:
+                st.text_area("Enter your text here:", key="text_area_Question 2", height=100)
+
+        elif page == "Question 3":
+            st.subheader("Question 3")
+            st.image("./image 3.png", caption="Image 3")
+            components.html(
+                """
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <title>Hello, World!</title>
+                        <style>
+                            body {
+                                display: flex;
+                                justify-content: flex-start;
+                            }
+                            iframe {
+                                margin: 0;
+                            }
+                        </style>
+                    </head>
+                    <body>
+                        <iframe width="700px" height="500px" src="https://www.programiz.com/c-programming/online-compiler/"></iframe>
+                    </body>
+                </html>
+                """,
+                height=500,
+                width=700,
+            )
+            fetch_latest_submission = submissions_collection.find_one({'user_id': user_id, 'submissions.question': 'Question 3'}, sort=[('submissions.timestamp', -1)])
+            if fetch_latest_submission:
+                st.text_area("Your previous answer:", value=fetch_latest_submission['submissions']['answer'], height=100, key="text_area_Question 3")
+            else:
+                st.text_area("Enter your text here:", key="text_area_Question 3", height=100)
+        if st.button("Submit"):
+            handle_submit(page, st.session_state[f"text_area_{page}"])
+        
+        def clear_input():
+            if user_input:  # Check if there is user input
+                    st.session_state[key].append(HumanMessage(content=user_input))
+                    with st.spinner("Thinking..."):
+                        response = gptModel(user_input, language, user_id)
+                    st.session_state[key].append(
+                        AIMessage(content=response))
+
+                    # Save chat history to a file
+                    chat_history = st.session_state.get(key, [])
+                    save_chat_history(user_id, chat_history)
+
+                    # Clear the input box after sending the message
+                    st.session_state[f"user_input_{user_id}"] = ""
+
 
         with st.sidebar:
             user_input = st.text_input("Your message: ", key=f"user_input_{user_id}")
 
-            if user_input:
-                st.session_state[key].append(HumanMessage(content=user_input))
-                with st.spinner("Thinking..."):
-                    response = gptModel(user_input)  # Use the gptModel function
-                st.session_state[key].append(
-                    AIMessage(content=response))
+            if st.sidebar.button("Send", on_click=clear_input):
+                pass
 
-                # Save chat history to a file
-                chat_history = st.session_state.get(key, [])
-                save_chat_history(user_id, chat_history)
-
-        messages = st.session_state.get(key, [])
-        for i, msg in enumerate(messages[1:]):
-            if i % 2 == 0:
-                message(msg.content, is_user=True, key=f"{i}_{user_id}_user")
-            else:
-                message(msg.content, is_user=False, key=f"{i}_{user_id}_ai")
+            messages = st.session_state.get(key, [])
+            for i, msg in enumerate(messages[1:]):
+                if i % 2 == 0:
+                    message(msg.content, is_user=True, key=f"{i}_{user_id}_user")
+                else:
+                    message(msg.content, is_user=False, key=f"{i}_{user_id}_ai")
 
 if __name__ == '__main__':
     main()
